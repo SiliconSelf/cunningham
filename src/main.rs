e2e289610e61385dbae46b13435b63732a0d7a3c
Wren Morozova <siliconself@proton.me>
2024-02-27 15:40:36 -0700

    Ran formatter

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ enum CodeMatrix {
     /// E9
     _E9,
     /// FF
-    _FF
+    _FF,
 }
 
 impl From<&str> for CodeMatrix {
@@ -51,7 +51,7 @@ impl From<&str> for CodeMatrix {
             3 => Self::_BD,
             4 => Self::_E9,
             5 => Self::_FF,
-            _ => unreachable!()
+            _ => unreachable!(),
         }
     }
 }
@@ -60,25 +60,37 @@ fn main() {
     let detection_model_data = include_bytes!("models/text-detection.rten");
     let rec_model_data = include_bytes!("models/text-recognition.rten");
 
-    let detection_model = Model::load(detection_model_data).expect("Couldn't create detection model");
-    let recognition_model = Model::load(rec_model_data).expect("Couldn't create recognition model");
+    let detection_model = Model::load(detection_model_data)
+        .expect("Couldn't create detection model");
+    let recognition_model =
+        Model::load(rec_model_data).expect("Couldn't create recognition model");
 
     let engine = OcrEngine::new(OcrEngineParams {
         detection_model: Some(detection_model),
         recognition_model: Some(recognition_model),
         ..Default::default()
-    }).expect("Couldn't create engine");
+    })
+    .expect("Couldn't create engine");
 
-    let image = read_image("res/matrixtest.jpg").expect("Couldn't load screenshot");
-    let ocr_input = engine.prepare_input(image.view()).expect("Couldn't prep OCR input");
+    let image =
+        read_image("res/matrixtest.jpg").expect("Couldn't load screenshot");
+    let ocr_input =
+        engine.prepare_input(image.view()).expect("Couldn't prep OCR input");
 
-    let word_rects = engine.detect_words(&ocr_input).expect("Couldn't detect words");
+    let word_rects =
+        engine.detect_words(&ocr_input).expect("Couldn't detect words");
     let line_rects = engine.find_text_lines(&ocr_input, &word_rects);
-    let line_texts = engine.recognize_text(&ocr_input, &line_rects).expect("Failed to recognize text");
+    let line_texts = engine
+        .recognize_text(&ocr_input, &line_rects)
+        .expect("Failed to recognize text");
 
     let mut code_matrix: Vec<Vec<CodeMatrix>> = Vec::new();
 
-    let lines = line_texts.iter().flatten().map(std::string::ToString::to_string).filter(|l| l.len() > 1);
+    let lines = line_texts
+        .iter()
+        .flatten()
+        .map(std::string::ToString::to_string)
+        .filter(|l| l.len() > 1);
     for line in lines {
         let mut matrix_line = Vec::new();
         for symbol in line.split(' ') {
